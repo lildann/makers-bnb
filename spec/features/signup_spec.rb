2f03a1f279a5613683e868957f963c0ff9b065dc
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-22 14:36:44 +0100

    Button linking sign up and log in page in case user already signed up

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 feature 'registration' do
-  scenario 'user can signup' do
+  scenario 'user can sign up' do
     visit('/')
     fill_in 'name', with: 'John'
     fill_in 'email', with: 'test@email.com'
     fill_in 'password', with: 'test_password'
-    click_button 'Submit'
+    click_button 'Sign up'
     expect(page).to have_content 'Welcome'
   end
 end
