144684777aaf4e382ee688ea650ead3fb93545b9
Lilly Daniell <daniell.lilly@gmail.com>
2021-09-23 11:26:24 +0100

    Final commit

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,5 @@ require 'pg'
 def setup_test_database
   p "Setting up test database.."
   connection = PG.connect(dbname: 'bnb_test') 
-  connection.exec('TRUNCATE users;')
+  connection.exec('TRUNCATE users, spaces;')
 end
